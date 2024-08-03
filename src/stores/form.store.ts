3b097f22fa84e41cf3c283a14b538b6c96e5c121
IGsayonara <diduniki@gmail.com>
2024-08-03 19:38:06 +0100

    Optimize watch algorithm

--- src/stores/form.store.ts
+++ src/stores/form.store.ts
@@ -1,7 +1,7 @@
 import { defineStore, storeToRefs } from 'pinia';
 import { useFormListStore } from '@/stores/form-list.store';
 import { useFormSearchStore } from '@/stores/form-search.store';
-import { watchEffect } from 'vue';
+import { onUnmounted, watch } from 'vue';
 
 import configService from '@/config/config';
 
@@ -14,14 +14,15 @@ export const useFormStore = defineStore('form', () => {
   const { inputs } = storeToRefs(formListStore);
   const { searchInput } = storeToRefs(formSearchStore);
 
-  watchEffect(() => {
-    const searchValue = searchInput.value.input.value;
+  let matchCount = 0;
 
-    let matchCount = 0;
+  // Watch for changes in the search input
+  watch(() => searchInput.value.input.value, newSearchValue => {
+    matchCount = 0;
 
     inputs.value.forEach(input => {
       const inputValue = input.input.value;
-      const isSearchMatch = searchValue && inputValue.includes(searchValue);
+      const isSearchMatch = newSearchValue && inputValue.includes(newSearchValue);
 
       if (isSearchMatch) {
         input.setColorValue(matchedInputColor);
@@ -36,6 +37,71 @@ export const useFormStore = defineStore('form', () => {
     } else {
       searchInput.value.setColorValue();
     }
+  });
+
+  // Store watchers to clean them up later
+  const inputWatchers = new Map();
+
+  // Function to set up watchers for each input
+  const setupInputWatchers = () => {
+    inputs.value.forEach(input => {
+      if (!inputWatchers.has(input)) {
+        const unwatch = watch(() => input.input.value, (newInputValue, oldInputValue) => {
+          const searchValue = searchInput.value.input.value;
+          const wasMatch = searchValue && oldInputValue.includes(searchValue);
+          const isMatch = searchValue && newInputValue.includes(searchValue);
+
+          if (isMatch) {
+            input.setColorValue(matchedInputColor);
+            if (!wasMatch) matchCount++;
+          } else {
+            input.setColorValue();
+            if (wasMatch) matchCount--;
+          }
+
+          if (matchCount === inputs.value.length && matchCount > 0) {
+            searchInput.value.setColorValue(matchedInputColor);
+          } else {
+            searchInput.value.setColorValue();
+          }
+        });
+
+        inputWatchers.set(input, unwatch);
+      }
+    });
+  };
+
+  // Watch for changes in the inputs array
+  watch(() => inputs.value.length, () => {
+    // Clean up watchers for removed inputs
+    inputWatchers.forEach((unwatch, input) => {
+      if (!inputs.value.includes(input)) {
+        const searchValue = searchInput.value.input.value;
+        const wasMatch = searchValue && input.input.value.includes(searchValue);
+        if (wasMatch) matchCount--;
+
+        unwatch();
+        inputWatchers.delete(input);
+      }
+    });
+
+    // Set up watchers for new inputs
+    setupInputWatchers();
+
+    // Update search input color if necessary
+    if (matchCount === inputs.value.length && matchCount > 0) {
+      searchInput.value.setColorValue(matchedInputColor);
+    } else {
+      searchInput.value.setColorValue();
+    }
+  });
+
+  // Initial setup of input watchers
+  setupInputWatchers();
+
+  // Clean up all watchers when the component is unmounted
+  onUnmounted(() => {
+    inputWatchers.forEach(unwatch => unwatch());
   });
 
   return {};
